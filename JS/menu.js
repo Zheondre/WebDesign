81156848442b5efafe0c3a995c3488da70172ee5
Master <zheondrecalcano@dhcp-129-63-182-22.uml.edu>
2015-09-29 18:42:29 -0400

    adding files for website to current dir

---
+++ JS/menu.js
@@ -0,0 +1,21 @@
+/* JS for menu, click on menu icon to have the menu slide out*/ 
+
+$(function() {
+    var pull        = $('#pull');
+        menu        = $('nav ul');
+        menuHeight  = menu.height();
+ 
+    $(pull).on('click', function(e) {
+        e.preventDefault();
+        menu.slideToggle();
+    });
+});
+$(window).resize(function(){
+    var w = $(window).width();
+    if(w > 320 && menu.is(':hidden')) {
+        menu.removeAttr('style');
+    }
+}); 
+
+
+
