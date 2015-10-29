bd10b57d59b0b134de31f1f98c9e01a71cb007dd
Angel M Calcano <acalcano@weblab.cs.uml.edu>
2015-10-29 18:19:28 -0400

    updating webpage

---
+++ p7/js/rules.js
@@ -0,0 +1,51 @@
+/* File:
+*  Angel_Calcano 
+*  
+*
+* http://stackoverflow.com/questions/15103289/jquery-validate-js-onkeyup-true-error
+*/
+
+$(document).ready(function() { 
+    $("#frm1").validate({
+	onkeyup: function(element) {$(element).valid()},
+	rules: { 
+	    minrow: { 
+		required: true, 
+		number: true
+	    },
+	    maxrow: {
+                required: true,
+                number: true
+            },
+	    mincol: {
+                required: true,
+                number: true
+            },
+	    maxcol: {
+                required: true,
+                number: true
+            }
+	}, 
+	messages: { 
+	    minrow: { 
+		required: "Enter a value for the minimum row value.",
+		digits: "Enter a digit."
+	    },
+	    maxrow: {
+                required: "Enter a value for the maxiimum row value.",
+                digits: "Enter a digit."
+            },
+	    mincol: {
+                required: "Enter a value for the minimum column value.",
+                digits: "Enter a digit."
+            },
+	    maxcol: {
+                required: "Enter a value for the maximum column value.",
+                digits: "Enter a digit."
+            }
+	}
+    });
+});
+		       
+
+
