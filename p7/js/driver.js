48d28f7d5d4f79d4e1cf6b444b9738df710d615b
Angel M Calcano <acalcano@weblab.cs.uml.edu>
2015-11-06 01:53:37 -0500

    fixed error messages

--- p7/js/driver.js
+++ p7/js/driver.js
@@ -1,59 +1,18 @@
-/* Angel Calcano
-*  js functions for assignment 6
+/* 
+   file:
+   Angel Calcano
+   91.461 Assignment: Using the jQuery Validation Plugin with Your Dynamic Table             
+   Angel Calcano, UMass Lowell Computer Science, Angel_Calcano@cs.uml.edu                    
+   Copyright (c) 2015 by Angel Calcano.  All rights reserved.  May be freely                 
+   copied or excerpted for educational purposes with credit to the author.
 *  Created 10/15/15
 *  Tutorials 
 *  http://pietschsoft.com/post/2006/06/01/Javascript-Loop-through-all-elements-in-a-form
-* Only numbers are allowed for input
-* http://stackoverflow.com/questions/7295843/allow-only-numbers-to-be-typed-in-a-textbox
 */
 
-// Tried fading the table in and out, doesn't work. 
-$(document).ready(function(){
-    $("button").click(function(){
-   
-        $("#tbl1").fadeToggle(1000);
-       // $("#div3").fadeToggle(3000);
-    });
-});
-
-/* Only positive numbers are allowed in the input                                   
-*  Check for duplicate numbers in row or col                                        
-*  Check for empty input.                                                           
-*/
 function validin(){ 
     var f1 = document.getElementById('frm1').elements; 
-/*
-    if( (f1[0].value > 10) || (f1[1].value > 10) || (f1[2].value > 10 )  || (f1[3].value > 10 )  ){
-	document.getElementById("tbl1").innerHTML = "";
-	return window.alert("Enter a range of ten.");
-    }*/
-    if( (f1[0].value === "") || (f1[1].value === "") || (f1[2].value === "")  || (f1[3].value === "")  ){ 
-	document.getElementById("tbl1").innerHTML = "";
-	return window.alert("Please enter a value in each field.");
-    } 
-
-    if( f1[0].value == f1[1].value){
-	document.getElementById("tbl1").innerHTML = "";
-	return window.alert("Row values can't be the same.");
-	
-    }
-    if(f1[2].value == f1[3].value){
-	document.getElementById("tbl1").innerHTML = "";
-	return window.alert("Column values can't be the same.");
-    }
-    else { 
-	colrow( f1[0].value, f1[1].value, f1[2].value, f1[3].value );  
-    }
-}
-
-function isNumber(evt) {
-    evt = (evt) ? evt : window.event;
-    var charCode = (evt.which) ? evt.which : evt.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-        window.alert("Invalid input, please enter a number.");
-	return false;
-    }
-    return true;
+    colrow( f1[0].value, f1[1].value, f1[2].value, f1[3].value );
 }
 
 // Build table
--- p7/js/rules.js
+++ p7/js/rules.js
@@ -1,96 +1,88 @@
 /* File:
-*  Angel_Calcano 
-*  
+*  91.461 Assignment: Using the jQuery Validation Plugin with Your Dynamic Table
+*  Angel Calcano, UMass Lowell Computer Science, Angel_Calcano@cs.uml.edu 
+*  Copyright (c) 2015 by Angel Calcano.  All rights reserved.  May be freely
+*  copied or excerpted for educational purposes with credit to the author.
+*  updated by AC on November 5, 2015. 10:00 am
 *
-* http://stackoverflow.com/questions/15103289/jquery-validate-js-onkeyup-true-error
+*  http://stackoverflow.com/questions/15103289/jquery-validate-js-onkeyup-true-error
 */
 
 $(document).ready(function() { 
     $("#frm1").validate({
+/*	$.validator.addMethod("rangelimit",function (value, element, param) {
+            return this.optional(element) || 10 < parseInt(value) - parseInt($(param).val());
+        });*/
 	onkeyup: function(element) {$(element).valid()},
 	rules: { 
 	    minrow: { 
 		required: true, 
-		number: true, 
-		range: [-1000,1000]
+		digits: true, 
+		range: [0,1000]
 	    },
 	    maxrow: {
                 required: true,
                 number: true,
-		range: [-1000,1000]
+		range: [0,1000]
+		//rangelimit: "#frm1" 
             },
 	    mincol: {
                 required: true,
                 number: true,
-		range: [-1000,1000]
+		range: [0,1000]
             },
 	    maxcol: {
                 required: true,
                 number: true,
-		range: [-1000,1000]
+		range: [0,1000]
             }
 	}, 
 	messages: { 
 	    minrow: { 
-		required: "Enter a value for the minimum row value.",
-		digits: "Enter a digit."
+		required: "The Minimum Row Value is required.",
+		digits: "Enter a digit for the Minimum Row Value."
 	    },
 	    maxrow: {
-                required: "Enter a value for the maxiimum row value.",
-                digits: "Enter a digit."
+                required: "The Maximum Row Value is required.",
+                number: "Enter a digit for the Maximum Row Value." 
+		//rangelimit:"Row range must be less than 10"
             },
 	    mincol: {
-                required: "Enter a value for the minimum column value.",
-                digits: "Enter a digit."
+                required: "The Minimum Column Value is required.",
+                number: "Enter a digit for the Minimum Column Value."
             },
 	    maxcol: {
-                required: "Enter a value for the maximum column value.",
-                digits: "Enter a digit."
+                required: "The Maximum Column Value is required.",
+                number: "Enter a digit for the Maximum Column Value."
             }
 	},
 	errorPlacement: function(error, element) { 
 	    switch( element.attr("name") ) { 
 	    case "minrow":
-		error.insertAfter("#erm");
+		error.appendTo("#erm");
 		break; 
 	    case "maxrow":
-		error.insertAfter("#erm");
+		error.appendTo("#erm1");
 		break; 
 	    case "mincol":
-		error.insertAfter("#erm");
+		error.appendTo("#erm2");
 		break;  
 	    case "maxcol":
-		error.insertAfter("#erm");
+		error.appendTo("#erm3");
 		break; 
 	    default:
-		error.insertAfter(element);
+		error.appendTo(element);
 	    } 
 	}
     });
 });
 /*
+$.validator.addMethod("rangelimit",function (value, element, param) { 
 
-errorPlacement: function(error, element) {
-            if ( element.attr("name") == "minrow" ) {
-                error.insertAfter("#erm");
-		}else {
-                error.insertAfter(element);
-		}
-		}
-*/
-
-
-/*
-jQuery.validator.addMethod("rangelimit",function (value, element) { 
-
-
+return this.optional(element) || 10 < parseInt(value) - parseInt($(param).val()); 
+} 'Invalid value'); 
 
 
 
 });
-
-
-
-
-
 */
