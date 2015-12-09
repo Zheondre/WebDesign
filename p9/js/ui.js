704144758c497fcaa0e8a46f148400ef0a439662
Angel M Calcano <acalcano@weblab.cs.uml.edu>
2015-12-09 13:10:11 -0500

    able to drop letters on board, now going to work on score feature

--- p9/js/ui.js
+++ p9/js/ui.js
@@ -1,48 +1,37 @@
 //https://jqueryui.com/sortable/#empty-lists
-/*$(function() {
-	$( "ul.droptrue" ).sortable({
-		connectWith: "ul"
-		    });
- 
-	$( "ul.dropfalse" ).sortable({
-		connectWith: "ul",
-		    dropOnEmpty: false
-		    });
- 
-	$( "#sortable1, #sortable2, #sortable3" ).disableSelection();
-    });*/
 $(function() {
-        $( "#sortable" ).sortable({
-            //revert: false                                                                
-        });
-        $( "#draggable" ).draggable({
-            connectToSortable: "#sortable",
-            helper: "clone"//,
-//revert: "invalid"
-});
-        $( "ul, li" ).disableSelection();
+    $( "#sortable" ).sortable({
+        //revert: false                                                                
     });
+    $( "#draggable" ).draggable({
+        connectToSortable: "#sortable",
+        helper: "clone"//,
+	//revert: "invalid"
+    });
+    $( "ul, li" ).disableSelection();
+});
 
 $(function() {
     $("td").droppable({
 	over:function( e, u ) { 
-	    console.log( $(this) );
+	    //console.log( $(this) );
 	    a = $(this);  
 	}, 
+	revert: "invalid",
 	accept: "#sortable > li",
 	activeClass: "ui-state-highlight",
 	drop: function( event, ui ) {
+//addscore();
             deleteImage( ui.draggable );
 	}
     });
     
     function deleteImage( $item ) {
-	console.log( $item ) 
+	console.log( a) 
 	$item.fadeOut(function() {
-	    
-	    
-	    $item.appendTo( a ).fadeIn(function() {
-          $item
+	     
+	$item.appendTo( a ).fadeIn(function() {
+		$item
 		    .animate({ width: "60px", height:"60px" })
 		    .find( "img" )
 		    .animate({ height: "60px",width: "60px" });
