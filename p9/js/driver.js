d84c5d8a151189cca4c926d07e0586e0d3f55cc4
Angel M Calcano <acalcano@weblab.cs.uml.edu>
2015-12-09 00:11:10 -0500

    able to set letters in the board

--- p9/js/driver.js
+++ p9/js/driver.js
@@ -7,105 +7,86 @@
  *  updated by JMH on November 27, 2015 at 10:22 AM to add original-distribution
  */
 $(document).ready(function() { 
-	var ScrabbleTiles = [] ;
-	ScrabbleTiles["A"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9  } ;
-	ScrabbleTiles["B"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
-	ScrabbleTiles["C"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
-	ScrabbleTiles["D"] = { "value" : 2,  "original-distribution" : 4,  "number-remaining" : 4  } ;
-	ScrabbleTiles["E"] = { "value" : 1,  "original-distribution" : 12, "number-remaining" : 12 } ;
-	ScrabbleTiles["F"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
-	ScrabbleTiles["G"] = { "value" : 2,  "original-distribution" : 3,  "number-remaining" : 3  } ;
-	ScrabbleTiles["H"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
-	ScrabbleTiles["I"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9  } ;
-	ScrabbleTiles["J"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1  } ;
-	ScrabbleTiles["K"] = { "value" : 5,  "original-distribution" : 1,  "number-remaining" : 1  } ;
-	ScrabbleTiles["L"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
-	ScrabbleTiles["M"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
-	ScrabbleTiles["N"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
-	ScrabbleTiles["O"] = { "value" : 1,  "original-distribution" : 8,  "number-remaining" : 8  } ;
-	ScrabbleTiles["P"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
-	ScrabbleTiles["Q"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1  } ;
-	ScrabbleTiles["R"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
-	ScrabbleTiles["S"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
-	ScrabbleTiles["T"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
-	ScrabbleTiles["U"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
-	ScrabbleTiles["V"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
-	ScrabbleTiles["W"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
-	ScrabbleTiles["X"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1  } ;
-	ScrabbleTiles["Y"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
-	ScrabbleTiles["Z"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1  } ;
-	ScrabbleTiles["_"] = { "value" : 0,  "original-distribution" : 2,  "number-remaining" : 2  } ;
-
-	$(function() {
-		$( "#sortable" ).sortable({
-			revert: true
-			    });
-		$( "#draggable" ).draggable({
-			connectToSortable: "#sortable",
-			    helper: "clone",
-			    revert: "invalid"
-			    });
-		$( "ul, li" ).disableSelection();
-	    });
-	// generates random char
-	function rchar(){
-	    // taking out _ from char array 11.8.15 11:13am doubled the amount of vowels to see more pop up in the rack
-	    var alpha= "AABCDEEFGHIIJKLMOONPQRSTUUVWXYYZ"; 
-	    return alpha.charAt( Math.floor(Math.random() * alpha.length ) );
-	} 
-	//should check for a case of when they are all zero
-	function draw(){ 
-	    var somechar = rchar();
-	    if ( ScrabbleTiles[somechar]["number-remaining"] == 0 ){
-		do { 
-		    somechar = rchar(); 
-		}while( ScrabbleTiles[somechar]["number-remaining"] == 0 )
-		    if( ScrabbleTiles[somechar]["number-remaining"] > 0 ){ 
-			ScrabbleTiles[somechar]["number-remaining"]--;
-			//return this object so return scrabbleTiles[somechar]
-			// instead of returning we can insert the info from here by getting the tag. of the span
-			return somechar; 
-		    }   
-	    }else {
-		ScrabbleTiles[somechar]["number-remaining"]--;    
-		// return ScrabbleTiles[somechar];
-		return somechar;
-
-	    }
+    var ScrabbleTiles = [] ;
+    ScrabbleTiles["A"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9  } ;
+    ScrabbleTiles["B"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+    ScrabbleTiles["C"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+    ScrabbleTiles["D"] = { "value" : 2,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+    ScrabbleTiles["E"] = { "value" : 1,  "original-distribution" : 12, "number-remaining" : 12 } ;
+    ScrabbleTiles["F"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+    ScrabbleTiles["G"] = { "value" : 2,  "original-distribution" : 3,  "number-remaining" : 3  } ;
+    ScrabbleTiles["H"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+    ScrabbleTiles["I"] = { "value" : 1,  "original-distribution" : 9,  "number-remaining" : 9  } ;
+    ScrabbleTiles["J"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+    ScrabbleTiles["K"] = { "value" : 5,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+    ScrabbleTiles["L"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+    ScrabbleTiles["M"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+    ScrabbleTiles["N"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+    ScrabbleTiles["O"] = { "value" : 1,  "original-distribution" : 8,  "number-remaining" : 8  } ;
+    ScrabbleTiles["P"] = { "value" : 3,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+    ScrabbleTiles["Q"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1  } ;
+    ScrabbleTiles["R"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+    ScrabbleTiles["S"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+    ScrabbleTiles["T"] = { "value" : 1,  "original-distribution" : 6,  "number-remaining" : 6  } ;
+    ScrabbleTiles["U"] = { "value" : 1,  "original-distribution" : 4,  "number-remaining" : 4  } ;
+    ScrabbleTiles["V"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+    ScrabbleTiles["W"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+    ScrabbleTiles["X"] = { "value" : 8,  "original-distribution" : 1,  "number-remaining" : 1  } ;
+    ScrabbleTiles["Y"] = { "value" : 4,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+    ScrabbleTiles["Z"] = { "value" : 10, "original-distribution" : 1,  "number-remaining" : 1  } ;
+    ScrabbleTiles["_"] = { "value" : 0,  "original-distribution" : 2,  "number-remaining" : 2  } ;
+    // generates random char
+    function rchar(){
+	// taking out _ from char array 11.8.15 11:13am doubled the amount of vowels to see more pop up in the rack
+	var alpha= "AABCDEEFGHIIJKLMOONPQRSTUUVWXYYZ"; 
+	return alpha.charAt( Math.floor(Math.random() * alpha.length ) );
+    } 
+    //should check for a case of when they are all zero
+    function draw(){ 
+	var somechar = rchar();
+	if ( ScrabbleTiles[somechar]["number-remaining"] == 0 ){
+	    do { 
+		somechar = rchar(); 
+	    }while( ScrabbleTiles[somechar]["number-remaining"] == 0 )
+	    if( ScrabbleTiles[somechar]["number-remaining"] > 0 ){ 
+		ScrabbleTiles[somechar]["number-remaining"]--;
+		return somechar; 
+	    }   
+	}else {
+	    ScrabbleTiles[somechar]["number-remaining"]--;    
+	    return somechar;    
 	}
-
-function addletter(somechar){
-    // creat the attribute 
-    pound = "." + somechar;
-    $('#sortable').append('<li class='+somechar+'></li>');
-    $(pound).attr("LValue", ScrabbleTiles[somechar]["value"]);
-    //$(pound).addclass("Lsize"); 
-}
-
-function rerack(){ 
-    for( var i = 0; i < 7; i++ ) {
-	addletter(draw());
+    }    
+    function addletter(somechar){
+	// creat the attribute 
+	pound = "." + somechar;
+	$('#sortable').append('<li class='+somechar+'></li>');
+	$(pound).attr("LValue", ScrabbleTiles[somechar]["value"]);
+	//$(pound).addclass("Lsize"); 
     }
-} 
-function clearRack(){ 
-
-}
-
-rerack();
-
-    });
+    function rerack(){ 
+	for( var i = 0; i < 7; i++ ) {
+	    addletter(draw());
+	}
+    } 
+    function clearRack(){ 
+    }
+    
+    rerack();
+    
+});
 $( "#submit" ).button().click(function() {
-        // check for a valid word
-	// add it to score
-	// clear both board and rack
-	// rerack()
-	
-    });
+    // check for a valid word
+    // add it to score
+    // clear both board and only add the amount that is needed
+    // rerack()
+    
+});
 $( "#reset" ).button().click(function() {
-	// increment the values that were decremented
-	// clear the rack
+    // increment the values that were decremented
+    // clear the rack
 	// rerack
-    });
+});
 $( "#quit" ).button().click(function() {
-    });
+});
 
--- p9/js/ui.js
+++ p9/js/ui.js
@@ -1,5 +1,5 @@
 //https://jqueryui.com/sortable/#empty-lists
-$(function() {
+/*$(function() {
 	$( "ul.droptrue" ).sortable({
 		connectWith: "ul"
 		    });
@@ -10,4 +10,46 @@ $(function() {
 		    });
  
 	$( "#sortable1, #sortable2, #sortable3" ).disableSelection();
+    });*/
+$(function() {
+        $( "#sortable" ).sortable({
+            //revert: false                                                                
+        });
+        $( "#draggable" ).draggable({
+            connectToSortable: "#sortable",
+            helper: "clone"//,
+//revert: "invalid"
+});
+        $( "ul, li" ).disableSelection();
     });
+
+$(function() {
+    $("td").droppable({
+	over:function( e, u ) { 
+	    console.log( $(this) );
+	    a = $(this);  
+	}, 
+	accept: "#sortable > li",
+	activeClass: "ui-state-highlight",
+	drop: function( event, ui ) {
+            deleteImage( ui.draggable );
+	}
+    });
+    
+    function deleteImage( $item ) {
+	console.log( $item ) 
+	$item.fadeOut(function() {
+	    
+	    
+	    $item.appendTo( a ).fadeIn(function() {
+          $item
+		    .animate({ width: "60px", height:"60px" })
+		    .find( "img" )
+		    .animate({ height: "60px",width: "60px" });
+            });
+	});
+    }  
+
+
+
+});
